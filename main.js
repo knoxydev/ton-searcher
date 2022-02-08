4883a0cda5137a82cff366ad2a5012fd7bb29409
nkr <nkgithubnarkuziev108@gmail.com>
2022-02-09 01:14:26 +0500

    Update

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let address = "EQAl_4uuCkI-rXUvdgbrk8jUmBrWrtZsGvmR3XxuSLC_cnRn";
 
-let url = `https://toncenter.com/api/v2/getWalletInformation?address=${address}`;
+//let url2 = `https://toncenter.com/api/v2/getWalletInformation?address=${address}`;
 
-let url2 = `https://toncenter.com/api/v2/getExtendedAddressInformation?address=${address}`
+let url = `https://toncenter.com/api/v2/getExtendedAddressInformation?address=${address}`
 
 async function send() {
-	let answerOne = await fetch(url2);
-	let resp1 = await answerOne.json();
+	let answerOne = await fetch(url);
+	let resp = await answerOne.json();
 
-	console.log(resp1);
+	let blc = resp.result.balance;
+
+	console.log(`Balance: ${blc}`)
+
+	let ton = blc * 10 ** (-9);
+
+	console.log(`Result: ${ton}`)
 }
 
 send();
