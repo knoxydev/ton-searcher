8e3dfc1ef5f4093b4957893a6fbd8678ca429ac3
nkr <nkgithubnarkuziev108@gmail.com>
2022-02-09 02:21:37 +0500

    Update

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-let address = "EQAl_4uuCkI-rXUvdgbrk8jUmBrWrtZsGvmR3XxuSLC_cnRn";
+async function sendRequest(address) {
+	let url = `https://toncenter.com/api/v2/getExtendedAddressInformation?address=${address}`;
 
-//let url2 = `https://toncenter.com/api/v2/getWalletInformation?address=${address}`;
-
-let url = `https://toncenter.com/api/v2/getExtendedAddressInformation?address=${address}`
-
-async function send() {
 	let answerOne = await fetch(url);
 	let resp = await answerOne.json();
 
-	let blc = resp.result.balance;
+	let ton = resp.result.balance * 10 ** (-9);
 
-	console.log(`Balance: ${blc}`)
+	console.log(`Result: ${ton}`);
+	console.log(resp.result);
 
-	let ton = blc * 10 ** (-9);
-
-	console.log(`Result: ${ton}`)
+	document.getElementById("wallet-address").innerHTML = resp.result.address.account_address;
+	document.getElementById("wallet-balance").innerHTML = `${ton} 💎`;
 }
 
-send();
+document.getElementById("main-search-input").addEventListener("focus", (e) => {
+	document.getElementById("main-search-input").addEventListener("keydown", (e) => start(e));
+});
+document.getElementById("main-search-input").addEventListener("focus", (e) => {
+	document.getElementById("main-search-input").removeEventListener("keydown", (e) => start(e));
+});
 
+let start = (e) => {
+	if (e.keyCode == 13) {
+		let inpText = document.getElementById("main-search-input").value;
+
+		if (inpText.charAt(inpText.length - 1) == " ") {
+			inpText = inpText.slice(0, -1);
+			document.getElementById("main-search-input").value = inpText;
+		}
+		//if (inpText == "") return createContent();
+
+		sendRequest(inpText);
+	}
+}
