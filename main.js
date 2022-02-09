990e1228ad92aa0a75041e1ddeeaeef1cf69aeb8
nkr <nkgithubnarkuziev108@gmail.com>
2022-02-10 02:33:57 +0500

    Add encode URI

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let wallet = {
+	"lt" : "",
+	"hash" : ""
+};
+
 async function getAddressInfo(address) {
 	let url = `https://toncenter.com/api/v2/getExtendedAddressInformation?address=${address}`;
 
@@ -5,13 +10,19 @@ async function getAddressInfo(address) {
 	let resp = await answerOne.json();
 	let ton = resp.result.balance * 10 ** (-9);
 
-	console.log(resp)
+	wallet['lt'] = resp.result.last_transaction_id['lt'];
+	wallet['hash'] = resp.result.last_transaction_id['hash'];
+
+	let encdURI = encodeURIComponent(wallet['hash']).replace(/['()]/g, escape).replace(/\*/g, '%2A').replace(/%(?:7C|60|5E)/g, unescape);
+	wallet['hash'] = encdURI;
+
+	console.log(wallet);
 
 	document.getElementById("wallet-address").innerHTML = resp.result.address.account_address;
 	document.getElementById("wallet-balance").innerHTML = `${ton} 💎`;
 }
 
-async function getAddress(address) {
+async function getAddressState(address) {
 	let url = `https://toncenter.com/api/v2/getAddressState?address=${address}`;
 
 	let answerOne = await fetch(url);
@@ -21,9 +32,11 @@ async function getAddress(address) {
 }
 
 async function getTransactions(address) {
-	let url = `https://toncenter.com/api/v2/getTransactions?address=${address}&limit=10&lt=24923013000003&hash=mcbrbKEU7oxVcRiWU9NL895or8S%2FiMbsIcfMX72weOo%3D&to_lt=0&archival=false`
-	
-	let answerOne = await fetch(url);
+	let url = `https://toncenter.com/api/v2/getTransactions?address=${address}&limit=10&lt=${wallet['lt']}&hash=${wallet['hash']}%3D&to_lt=0&archival=false`
+	let url2 = `https://toncenter.com/api/v2/getTransactions?address=${address}&limit=10&lt=${wallet['lt']}&hash=${wallet['hash']}&to_lt=0&archival=false`;
+
+
+	let answerOne = await fetch(url2);
 	let resp = await answerOne.json();
 
 	console.log(resp);
@@ -37,7 +50,7 @@ document.getElementById("main-search-input").addEventListener("focus", (e) => {
 	document.getElementById("main-search-input").removeEventListener("keydown", (e) => start(e));
 });
 
-let start = (e) => {
+async function start(e) {
 	if (e.keyCode == 13) {
 		let inpText = document.getElementById("main-search-input").value;
 
@@ -47,8 +60,8 @@ let start = (e) => {
 		}
 		//if (inpText == "") return createContent();
 
-		getAddressInfo(inpText);
-		getAddress(inpText);
-		getTransactions(inpText);
+		await getAddressInfo(inpText);
+		await getAddressState(inpText);
+		await getTransactions(inpText);
 	}
 }
